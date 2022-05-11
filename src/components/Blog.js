96c73934e954713e8e64fab1e824790ed66ad856
Gianluca Cavajani <cavajanig@gmail.com>
2022-05-11 17:45:25 -0300

    ex 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+const Blog = ({ blog }) => {
+  const [hide, setHide] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  // const hideWhenVisible = { display: visible ? 'none' : '' };
+  // const showWhenVisible = { display: visible ? '' : 'none' };
+
+  if (hide) {
+    return (
+      <div style={blogStyle}>
+        <strong>{blog.title}</strong>
+        <button onClick={() => setHide(false)}>close</button>
+        <p>author: {blog.author}</p>
+        <p>url: {blog.url}</p>
+        <p>
+          likes:
+          {blog.likes}
+          <button>like</button>
+        </p>
+      </div>
+    );
+  }
+  return (
+    <div style={blogStyle}>
+      <p>
+        <strong>{blog.title}</strong>{' '}
+        <button onClick={() => setHide(true)}>show</button>
+      </p>
+    </div>
+  );
+};
 
 export default Blog;
